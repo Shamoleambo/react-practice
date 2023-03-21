97406053adf6b7afc7e7f49a48a111198b23bf8d
Tiago Gomes <tidgomes@gmail.com>
2023-03-21 09:24:53 -0300

    make AddUser a stateful component and create the functions which handle the state

--- src/Users/AddUser.js
+++ src/Users/AddUser.js
@@ -1,19 +1,40 @@
+import { useState } from 'react'
 import Card from '../UI/Card'
 import Button from '../UI/Button'
 import styles from './AddUser.module.css'
 
 const AddUser = props => {
+  const [nameInput, setNameInput] = useState('')
+  const [ageInput, setAgeInput] = useState('')
+
   const addUserHandler = event => {
     event.preventDefault()
+    console.log(nameInput, ageInput)
+  }
+  const userNameChangeHandler = event => {
+    setNameInput(event.target.value)
+  }
+  const userAgeChangeHandler = event => {
+    setAgeInput(event.target.value)
   }
 
   return (
     <Card className={styles.input}>
       <form onSubmit={addUserHandler}>
         <label htmlFor='username'>User Name</label>
-        <input type='text' id='username' />
+        <input
+          type='text'
+          id='username'
+          value={nameInput}
+          onChange={userNameChangeHandler}
+        />
         <label htmlFor='age'>Age (Years)</label>
-        <input type='number' id='age' />
+        <input
+          type='number'
+          id='age'
+          value={ageInput}
+          onChange={userAgeChangeHandler}
+        />
         <Button type='submit'>Add User</Button>
       </form>
     </Card>
