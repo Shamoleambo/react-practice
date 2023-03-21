9abfe0a19873207a5e8ab280be1d83ed37f02719
Tiago Gomes <tidgomes@gmail.com>
2023-03-21 12:17:18 -0300

    create an overlay-root and a backdrop-root to use portal from ReactDOM to render the ErrorModal

--- src/components/UI/ErrorModal.js
+++ src/components/UI/ErrorModal.js
@@ -1,22 +1,43 @@
+import ReactDOM from 'react-dom'
 import Card from './Card'
 import Button from './Button'
 import styles from './ErrorModal.module.css'
 
+const Backdrop = props => {
+  return <div className={styles.backdrop} onClick={props.onDismissError}></div>
+}
+
+const ModalOverlay = props => {
+  return (
+    <Card className={styles.modal}>
+      <header className={styles.header}>
+        <h2>{props.title}</h2>
+      </header>
+      <div className={styles.content}>
+        <p>{props.message}</p>
+      </div>
+      <footer className={styles.actions}>
+        <Button onClick={props.onDismissError}>OK</Button>
+      </footer>
+    </Card>
+  )
+}
+
 const ErrorModal = props => {
   return (
     <>
-      <div className={styles.backdrop} onClick={props.onDismissError}></div>
-      <Card className={styles.modal}>
-        <header className={styles.header}>
-          <h2>{props.title}</h2>
-        </header>
-        <div className={styles.content}>
-          <p>{props.message}</p>
-        </div>
-        <footer className={styles.actions}>
-          <Button onClick={props.onDismissError}>OK</Button>
-        </footer>
-      </Card>
+      {ReactDOM.createPortal(
+        <Backdrop onDismissError={props.onDismissError} />,
+        document.getElementById('backdrop-root')
+      )}
+      {ReactDOM.createPortal(
+        <ModalOverlay
+          title={props.title}
+          message={props.message}
+          onDismissError={props.onDismissError}
+        />,
+        document.getElementById('overlay-root')
+      )}
     </>
   )
 }
