28d5418b2371b00e645745ccf2e304ad64e6c4e7
Tiago Gomes <tidgomes@gmail.com>
2023-03-21 08:57:22 -0300

    create AddUser component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
-import React from 'react';
-
+import AddUser from './Users/AddUser'
 
 function App() {
   return (
     <div>
-
+      <AddUser />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
